ba6cb3576404e6433dca062c061791d7a4f7c1ff
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2021-02-25 13:57:45 +0100

    correct bugs, add toggleView function

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hello world xd");
+const div = document.querySelector("div");
+const h1 = document.querySelector("h1");
+let rocks = true;
+
+const toggleView = () => {
+    div.style.backgroundImage = rocks ? "url(moon.jpg)" : "url(rocks.jpg)";
+    h1.textContent = rocks ? "Its dark.." : "Sun is shining :)";
+    rocks = !rocks;
+}
+
+div.addEventListener("click", toggleView);
