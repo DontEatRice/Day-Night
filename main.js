88cff47f0158f1c0dc0d836be7af0dcab2af7727
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2021-02-27 10:11:09 +0100

    add preloading images

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const div = document.querySelector("div");
 const h1 = document.querySelector("h1");
-let rocks = true;
+let bool = true;
+const moon = new Image();
+moon.src = './moon.jpg'
+
+const rocks = new Image();
+rocks.src = './rocks.jpg'
 
 const toggleView = () => {
-    div.style.backgroundImage = rocks ? "url(moon.jpg)" : "url(rocks.jpg)";
-    h1.textContent = rocks ? "Its dark.." : "Sun is shining :)";
-    rocks = !rocks;
+    div.style.backgroundImage = bool ? `url(${moon.src})` : `url(${rocks.src})`;
+    h1.textContent = bool ? "Its dark.." : "Sun is shining :)";
+    bool = !bool;
 }
 
 div.addEventListener("click", toggleView);
